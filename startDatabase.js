e8c522649153ad2aaeefa7e8c8cd3acf7ab53eb5
Khuu Thanh <khuuthanh@super.local>
2018-05-23 16:53:24 +0700

    fix

--- startDatabase.js
+++ startDatabase.js
@@ -4,7 +4,6 @@ mongoose.Promise = global.Promise;
 const localUri = 'mongodb://localhost/eshopper';
 function getDatabaseUri() {
     if(process.env.NODE_ENV === 'production') {
-        initDatabase();
         return 'mongodb://eshopper:123321@ds157682.mlab.com:57682/thanhsuper'
     }
     return localUri
